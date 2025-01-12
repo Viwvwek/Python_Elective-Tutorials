d5a79f35635d3f9d6472857818dde7b1cf6c846e
VIVEK VINOD <vivekvinod4567@gmail.com>
2025-01-12 10:06:06 +0530

    Improve ROI calculator documentation and formatting

--- Tutorial 2/Investment_report.py
+++ Tutorial 2/Investment_report.py
@@ -1,14 +1,20 @@
 """
-    "Return of investment"(ROI) calculator
+    "Return on Investment" (ROI) Calculator
     
-    It  return ((current_value - investment_amount) / investment_amount) * 100
-    reads both:
+    It calculates ROI as:
+        ((current_value - investment_amount) / investment_amount) * 100
+    Reads both:
         current_value and investment_amount
 """
-#reading the current_value
-current_value = input("Enter the Current_Value:")
-#reading the investment_amount
-investment_amount = input("Enter the Investment_Amount:")
-#calculating the ROI  
-roi = ((float(current_value)-float(investment_amount))/float(investment_amount))*100
-print("ROI is:", roi)
+
+# Reading the current_value
+current_value = input("Enter the Current Value: ")
+
+# Reading the investment_amount
+investment_amount = input("Enter the Investment Amount: ")
+
+# Calculating the ROI
+roi = ((float(current_value) - float(investment_amount)) / float(investment_amount)) * 100
+
+# Displaying the ROI
+print("ROI is: {:.2f}%".format(roi))
