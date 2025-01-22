abe172e958c13b0990d7cdee22d4e7824eb8fb50
VIVEK VINOD <vivekvinod4567@gmail.com>
2025-01-22 23:17:31 +0530

    binary_decimal

---
+++ Tutorial 5/Tut_5.py
@@ -0,0 +1,8 @@
+"""
+    This is the python code for
+        ---Binary to Decimal conversion
+           using Bin()
+"""
+
+input_number = int(input("Enter the input_decimal: "))
+print("The binary value of the",input_number,"is",bin(input_number) )
